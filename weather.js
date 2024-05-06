8b8d2327c168316dcfd4c0238e153003ba631f09
Benjamin Dargue <b2dargue@gmail.com>
2024-05-06 05:53:55 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,2 +1,2 @@
-if (a == "bobbyboucher150"){$(urlfetch https://wttr.in/Eugene?&format=%t+%c+%C)}
-else {$(urlfetch https://wttr.in/Eugene?u&format=%t+%c+%C)}
+if (a == "bobbyboucher150"){"$(urlfetch https://wttr.in/Eugene?&format=%t+%c+%C)"}
+else {"$(urlfetch https://wttr.in/Eugene?u&format=%t+%c+%C)"}
